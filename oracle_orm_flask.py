d289c983eb776ffb10984af6963d6198fdaf1066
gollapudisai18 <113593313+gollapudisai18@users.noreply.github.com>
2023-01-23 16:15:17 +0530

    Update oracle_orm_flask.py

--- oracle_orm_flask.py
+++ oracle_orm_flask.py
@@ -2,7 +2,7 @@ from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'oracle://sai:kiran@localhost:1521/xe'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'oracle://username:password@localhost:1521/xe'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
